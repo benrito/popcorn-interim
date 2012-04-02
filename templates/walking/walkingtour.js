0d32e64692d5d6e8ca0db3ed7da5c17841765e2a
Ben Moskowitz <benrito@gmail.com>
2012-04-02 02:43:12 -0700

    Added support files for templates

---
+++ templates/walking/walkingtour.js
@@ -0,0 +1,39 @@
+document.addEventListener( "DOMContentLoaded", function( e ){
+
+  Butter({
+    config: "../../butter/config/complete.conf",
+    ready: function( butter ){
+      var media = butter.media[ 0 ];
+
+      function start(){
+        var track = media.addTrack( "Track1" );
+        media.addTrack( "Track" + Math.random() );
+        media.addTrack( "Track" + Math.random() );
+
+        var event = track.addTrackEvent({
+          type: "text",
+          popcornOptions: {
+            start: 0,
+            end: 3,
+            text: "test",
+            target: "tour_area"
+          }
+        });
+
+        butter.tracks[ 2 ].addTrackEvent({ 
+          type: "footnote",
+          popcornOptions: {
+            start: 1,
+            end: 2,
+            text: "This is where you can put footnotes",
+            target: "chapter_title"
+          }
+        });
+      }
+
+      media.onReady( start );
+      
+      window.butter = butter;
+    } 
+  }); //Butter
+}, false );
